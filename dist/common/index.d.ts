d45d697f656abec1639b415c64fd4eb3f457736d
xucongli1989 <80213876@qq.com>
2022-05-23 18:08:08 +0800

    build: build

---
+++ dist/common/index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * 判断是否为生产环境（process.env.NODE_ENV==production）
+ */
+export declare function isProduction(): boolean;
+/**
+ * 判断是否为开发环境（process.env.NODE_ENV==development）
+ */
+export declare function isDevelopment(): boolean;
+//# sourceMappingURL=index.d.ts.map
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,10 @@
 import * as encode from "./encode";
 import * as file from "./file";
+import * as common from "./common";
 declare const _default: {
     encode: typeof encode;
     file: typeof file;
+    common: typeof common;
 };
 export default _default;
 //# sourceMappingURL=index.d.ts.map
